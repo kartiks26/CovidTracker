265d40378ed07ea1c29ee5379c41737bec8e9b0d
pratikartik <kartikshikhare26@gmail.com>
2021-09-08 22:26:21 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Template</h1>
+				<p>Professor(pratikartik)</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
